b60d76f130c11755f3a2de6ac34cf32eecc4f942
nummad <nummad222@gmail.com>
2025-01-08 10:09:29 +0500

    comit

--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -3,32 +3,10 @@ import Logo from '../images/logoWhite.jpg';
 import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
-    const bubbles = Array.from({ length: 128 }, (_, i) => {
-        const size = 2 + Math.random() * 4;
-        const distance = 6 + Math.random() * 4;
-        const position = -5 + Math.random() * 110;
-        const time = 2 + Math.random() * 2;
-        const delay = -1 * (2 + Math.random() * 2);
-
-        return (
-          <div
-            key={i}
-            className={style.bubble}
-            style={{
-              "--size": `${size}px`,
-              "--distance": `${distance}px`,
-              "--position": `${position}%`,
-              "--time": `4s`,
-              "--delay": `2s`,
-            }}
-          ></div>
-        );
-      });
     const navigate = useNavigate();
     const currentYear = new Date().getFullYear();
     return (
         <div>
-        <div className={style.bubbles}>{bubbles}</div>
         <div className={style.footer}>
             <div className={style.hiddenFooter}>
                 <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
